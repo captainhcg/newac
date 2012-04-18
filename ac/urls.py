854dc9660d2148a568e6a5c1d90fb8f8b120dbc1
Chongguang He <captainhcg@gmail.com>
2012-04-18 00:20:07 -0500

    start working on search

--- ac/urls.py
+++ ac/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('ac.index.urls')),
+    url(r'^$', include('index.urls')),
     # Examples:
     # url(r'^$', 'ac.views.home', name='home'),
     # url(r'^ac/', include('ac.foo.urls')),
@@ -16,3 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
--- index/views.py
+++ index/views.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
 # Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.utils import timezone
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.contrib import staticfiles
+
+
+def index(request):
+    title = u"AC的新衣"
+    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
