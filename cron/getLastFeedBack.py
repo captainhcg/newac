ae92eef45cb404f257e99f5e8ade93dbfc59584f
Chongguang He <captainhcg@gmail.com>
2012-04-21 02:04:17 -0500

    working on spider

--- cron/getLastFeedBack.py
+++ cron/getLastFeedBack.py
@@ -4,8 +4,9 @@
 from xml.dom.minidom import parseString
 from HTMLParser import HTMLParser
 import core, json
+from core import cache
 
-def update_list(ls):
+def update_dict(dic):
     new_feeds = core.grabPage("www.acfun.tv", "/api/getlastfeedback.aspx");
     if new_feeds:
         new_feeds_list = json.loads(new_feeds)
@@ -15,24 +16,44 @@ def update_list(ls):
     for new_feed in new_feeds_list['feedbacks']:
         item = {}
         item['title'] = new_feed['title']
-        item['desc'] = new_feed['description']
+        try:
+            item['desc'] = new_feed['description']
+        except KeyError:
+            item['desc'] = ""
         item['link'] = new_feed['contentURL']
         item['id'] = new_feed['aid']
         item['time'] = int(new_feed['feedbackTime'])
-        ls[item['id']] = item
-        print item
+        dic[item['id']] = item
 
-    ls.sort(ls, key=lambda x: x['time'], reverse=True)
-    print ls
+    items = []
+    for k, v in dic.iteritems():
+        items.append(v)
+    
+    items.sort(articleComparator)
+    new_dict = {}
+
+    count = 1;
+    for item in items:
+        new_dict[item['id']] = item
+        if ++count > 100:
+            break
+
+    cache.set("getlastfeedback", new_dict, 3600*24)
     return
 
+def articleComparator(i1, i2):
+    if i2['time'] > i1['time']:
+        return 1
+    elif i2['time'] == i1['time']:
+        return 0
+    return -1
+
 def main():
-    memc = core.getMemc()
-    if memc.get("getlastfeedback"):
-        last_articles = memc.get("getlastfeedback")
+    if cache.get("getlastfeedback"):
+        last_articles = cache.get("getlastfeedback")
     else:
         last_articles = {}
-    update_list(last_articles)
+    update_dict(last_articles)
 
 if __name__ == "__main__":
     main()
---
+++ cron/getLastFeedBack.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH=$HOME/workspace/newac/:$PYTHONPATH
+export DJANGO_SETTINGS_MODULE=ac.settings
+
+/home/snake/workspace/newac/cron/./getLastFeedBack.py
+
